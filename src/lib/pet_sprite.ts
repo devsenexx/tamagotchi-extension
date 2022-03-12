50230e63f7d874a7ea9a318a2fc20123b873ca7d
Chen Asraf <chenasrafil@gmail.com>
2022-03-13 01:52:13 +0200

    cleanup

--- src/lib/pet_sprite.ts
+++ src/lib/pet_sprite.ts
@@ -3,12 +3,22 @@ import PetData, { PetState } from "../lib/pet_data"
 import { useTick } from "./tick"
 import { useCanvas } from "./sprite_hooks"
 
-const MOD = 500
-const SPEED = 40
-const SPRITE_SIZE = 24
+const SPRITE_WIDTH = 24
+const SPRITE_HEIGHT = 24
 const BG_SIZE = 32
-const PET_FRAME_COUNT = 2
-const DROPPING_FRAME_COUNT = 3
+
+const frameCounts: Partial<Record<PetState, number>> = {
+  idle: 2,
+  moving: 9,
+}
+const frameSpeeds: Partial<Record<PetState, number>> = {
+  idle: 40,
+  moving: 2,
+}
+const frameSlices: Partial<Record<PetState, number>> = {
+  idle: 0,
+  moving: 1,
+}
 
 export function usePetSprite({
   ctx,
@@ -29,25 +39,13 @@ export function usePetSprite({
   lockState?: boolean
 }) {
   const { frame } = useTick()
-  const frameCounts: Partial<Record<PetState, number>> = {
-    idle: 2,
-    moving: 9,
-  }
-  const frameSpeeds: Partial<Record<PetState, number>> = {
-    idle: 40,
-    moving: 2,
-  }
-  const frameSlices: Partial<Record<PetState, number>> = {
-    idle: 0,
-    moving: 1,
-  }
   const animKey: PetState = lockState ? "idle" : frameCounts[pet.state] ? pet.state : "idle"
   const petAnimFrame = React.useMemo(
     () => Math.floor(frame / frameSpeeds[animKey]) % frameCounts[animKey],
     [frame, animKey]
   )
   const bgAnimFrame = React.useMemo(
-    () => Math.floor(((frame / SPEED) * 2) % (width * 2)) - width,
+    () => Math.floor(((frame / frameSpeeds.idle!) * 2) % (width * 2)) - width,
     [frame]
   )
 
@@ -70,10 +68,10 @@ export function usePetSprite({
       if (pet.spriteImage) {
         ctx.drawImage(
           pet.spriteImage,
-          petAnimFrame * SPRITE_SIZE,
-          frameSlices[animKey] * SPRITE_SIZE,
-          SPRITE_SIZE,
-          SPRITE_SIZE,
+          petAnimFrame * SPRITE_WIDTH,
+          frameSlices[animKey] * SPRITE_WIDTH,
+          SPRITE_WIDTH,
+          SPRITE_HEIGHT,
           padding,
           padding,
           width - padding * 2,
